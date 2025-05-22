0c50152aa3c748d1e23aaf45ae4ebdecc5fa647c
Mhoraya <job.mate06@gmail.com>
2025-05-22 15:12:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+const toggleBtn = document.getElementById('theme-toggle');
+const icon = toggleBtn.querySelector('i');
+
+// Betöltéskor ellenőrizzük, van-e mentett téma
+window.addEventListener("load", () => {
+  // Preloader eltüntetése
+  const preloader = document.getElementById("preloader");
+  if (preloader) {
+    preloader.classList.add("hide");
+    setTimeout(() => {
+      preloader.style.display = "none";
+    }, 400);
+  }
+
+  // Téma betöltése localStorage-ból
+  const savedTheme = localStorage.getItem('theme');
+  const isDark = savedTheme === 'dark';
+  if (isDark) {
+    document.body.classList.add('dark-mode');
+    icon.className = 'fa-solid fa-moon';
+  } else {
+    icon.className = 'fa-solid fa-sun';
+  }
+});
+
+// Gombra kattintás: téma váltása + mentés
+toggleBtn.addEventListener('click', () => {
+  const isDark = document.body.classList.toggle('dark-mode');
+  icon.className = isDark ? 'fa-solid fa-moon' : 'fa-solid fa-sun';
+  localStorage.setItem('theme', isDark ? 'dark' : 'light');
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const tooltip = document.getElementById('tooltip');
+  let verses = {};
+
+  fetch('../verses.json')
+    .then(res => res.json())
+    .then(data => {
+      verses = data;
+
+      document.querySelectorAll('.scripture').forEach(el => {
+        el.addEventListener('mouseenter', () => {
+          const key = el.getAttribute('data-ref');
+          tooltip.textContent = verses[key] || 'Nincs magyarázat';
+
+          tooltip.style.display = 'block';
+
+          const rect = el.getBoundingClientRect();
+          tooltip.style.left = `${rect.left + window.scrollX}px`;
+          tooltip.style.top = `${rect.bottom + window.scrollY + 5}px`;
+        });
+
+        el.addEventListener('mouseleave', () => {
+          tooltip.style.display = 'none';
+        });
+      });
+    })
+    .catch(err => {
+      console.error('Nem sikerült betölteni a verses.json-t', err);
+    });
+});
